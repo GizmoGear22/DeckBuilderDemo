7cb82a89bcca4bf7b9702ce6ea21ab4011ee4d8a
GizmoGear22 <mpsimmer@gmail.com>
2024-06-09 10:54:04 -0400

    Starting Project. Setting up basic framework

---
+++ DBAccess/DBControllers/AvailableCardsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace DBAccess.DBControllers
+{
+	public class AvailableCardsController
+	{
+		private readonly IConfiguration _configuration;
+		public AvailableCardsController(IConfiguration configuration) 
+		{
+			_configuration = configuration;
+		}
+		public string CnnVal()
+		{
+			return _configuration.GetConnectionString("DBAccessHandler");
+		}
+	}
+}
