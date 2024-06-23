d55430d330a45924452e46454a81291b6e2b0947
GizmoGear22 <mpsimmer@gmail.com>
2024-06-23 14:28:23 -0400

    Added a couple unit tests for Get and Post

---
+++ UnitTests/LogicLayerTests/APIGetHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicLayer.APIGetLogic;
+using Moq;
+using Models;
+using Xunit;
+
+namespace UnitTests.LogicLayerTests
+{
+	public class APIGetHandlerTests
+	{
+		[Fact]
+		public async Task GetCardsToAPITest()
+		{
+			//arrange
+			var sample = new SampleCardLists();
+			var sampleList = sample.SampleList();
+			var mock = new Mock<IDBGetHandlers>();
+			var data = mock.Setup(x => x.GetAllCardsFromRepository()).ReturnsAsync(new List<CardModel>(sampleList));
+
+			//act
+			var handler = new APIGetHandlers(mock.Object);
+			var result = await handler.GetAllCards();
+
+			//assert
+			Assert.NotNull(result);
+			Assert.Equal(result, sampleList);
+		}
+	}
+}
---
+++ UnitTests/LogicLayerTests/APIPostHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using Models;
+using LogicLayer.APIPostLogic;
+using System.Security.Cryptography.X509Certificates;
+using LogicLayer.Validation;
+
+namespace UnitTests.LogicLayerTests
+{
+	public class APIPostHandlerTests
+	{
+		private readonly Mock<IDBPostHandlers> _postHandlers;
+		private readonly Mock<IIdValidation> _idValidation;
+		public APIPostHandlerTests() 
+		{
+			_postHandlers = new Mock<IDBPostHandlers>();
+			_idValidation = new Mock<IIdValidation>();
+		}
+		[Fact]
+		public void APIPostHandlerTest()
+		{
+			//arrange
+			var model = new CardModel
+			{
+				id = 1,
+				name = "Machine Rifle"
+			};
+
+			_idValidation.Setup(x => x.CheckIfIdExists(model)).ReturnsAsync(true);
+			var handler = new APIPostHandler(_postHandlers.Object, _idValidation.Object);
+
+			//act
+			var result = handler.PostNewCard(model);
+
+			//assert
+			_postHandlers.Verify(x => x.DBPostHandler(model), Times.Once);
+		}
+	}
+}
